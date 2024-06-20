c7df690a189d86338e471e1632641b849565a70c
Gonzalo Galvan <gonzalongalvan2@gmail.com>
2024-06-19 21:17:28 -0300

    first commit

---
+++ datos.js
@@ -0,0 +1,9 @@
+let nombre
+let apellido
+let edad
+
+nombre = prompt("Ingrese su nombre")
+apellido = prompt("Ingrese su edad")
+edad = parseInt(prompt("Ingrese su edad (años)"))
+
+alert(`Hola ${nombre} ${apellido}, tu edad es de ${edad} años`)
